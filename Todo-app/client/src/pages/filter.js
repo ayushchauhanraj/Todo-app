dc5174e989eb98be589bb31d874d333c89232178
Ayush Chauhan <52425077+ayushchauhanraj@users.noreply.github.com>
2020-06-07 11:13:42 +0530

    Update filter.js

--- Todo-app/client/src/pages/filter.js
+++ Todo-app/client/src/pages/filter.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
 import { visibilityFilter } from "../store/actions";
 import { makeStyles } from "@material-ui/core/styles";
@@ -6,7 +6,6 @@ import Button from "@material-ui/core/Button";
 import ArchiveIcon from "@material-ui/icons/Archive";
 import TimelapseIcon from "@material-ui/icons/Timelapse";
 import CheckCircleIcon from "@material-ui/icons/CheckCircle";
-import ButtonGroup from "@material-ui/core/ButtonGroup";
 import WorkIcon from "@material-ui/icons/Work";
 import AssignmentIndIcon from "@material-ui/icons/AssignmentInd";
 import AllInboxIcon from "@material-ui/icons/AllInbox";
@@ -14,10 +13,12 @@ import AllInboxIcon from "@material-ui/icons/AllInbox";
 const useStyles = makeStyles((theme) => ({
   root: {
     display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "center",
+
     "& > *": {
-      marginTop: 4,
+      marginTop: 10,
       marginBottom: 4,
     },
   },
@@ -26,63 +27,51 @@ const useStyles = makeStyles((theme) => ({
 const Filter = (props) => {
   const classes = useStyles();
 
-  return (
-    <div className={classes.root}>
-      <ButtonGroup
-        color="primary"
-        size="small"
-        aria-label="outlined primary button group"
-      >
-        <Button
-          color="primary"
-          size="small"
-          onClick={() => props.visibility("SHOW_ALL")}
-        >
-          <AllInboxIcon />
-          All
-        </Button>
-        <Button
-          color="primary"
-          size="small"
-          onClick={() => props.visibility("COMPLETE")}
-        >
-          <CheckCircleIcon /> Completed
-        </Button>
-        <Button
-          color="primary"
-          size="small"
-          onClick={() => props.visibility("PENDING")}
-        >
-          <TimelapseIcon /> Pending
-        </Button>
+  const [selectedItem, setSelectedItem] = useState(0);
 
-        <Button
-          color="primary"
-          size="small"
-          onClick={() => props.visibility("WORK")}
-        >
-          <WorkIcon />
-          Work
-        </Button>
-        <Button
-          color="primary"
-          size="small"
-          onClick={() => props.visibility("PERSONAL")}
-        >
-          <AssignmentIndIcon />
-          Personal
-        </Button>
-        <Button
-          color="primary"
-          size="small"
-          onClick={() => props.visibility("ARCHIVE")}
-        >
-          <ArchiveIcon />
-          Archived
-        </Button>
-      </ButtonGroup>
-    </div>
-  );
+  const buttonSelected = (selButton) => {
+    setSelectedItem(selButton);
+  };
+  useEffect(() => {
+    props.visibility("SHOW_ALL");
+  }, []);
+
+  const [value, setValue] = useState([
+    "SHOW_ALL",
+    "COMPLETE",
+    "PENDING",
+    "PERSONAL",
+    "WORK",
+    "ARCHIVE",
+  ]);
+
+  const [icon] = useState([
+    <AllInboxIcon />,
+    <CheckCircleIcon />,
+    <TimelapseIcon />,
+    <AssignmentIndIcon />,
+    <WorkIcon />,
+    <ArchiveIcon />,
+  ]);
+
+  const buttons = value.map((val, i) => (
+    <Button
+      size="small"
+      color="primary"
+      id={i}
+      key={i}
+      variant={i === selectedItem ? "contained" : "outlined"}
+      onClick={() => {
+        props.visibility(value[i]);
+        buttonSelected(i);
+      }}
+    >
+      {icon[i]}
+      {val}
+    </Button>
+  ));
+
+  return <div className={classes.root}>{buttons}</div>;
 };
 
 const MapDisptachtoProps = (dispatch) => {
